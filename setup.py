cac9b223aa48b347116566f92985404fa38b994e
sanket sancheti <sanketsancheti752@gmail.com>
2024-12-02 17:52:12 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List
+
+hypen = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements'''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if hypen in requirements:
+            requirements.remove(hypen)
+    return requirements
+
+
+
+
+setup(
+    name = "mlproject",
+    version = '0.0.1',
+    author= 'sanket',
+    author_email= 'sanketsancheti@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+
+
+)
